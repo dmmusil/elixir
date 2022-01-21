3151685452348c8d5b318e2f429a853fc49bd5ef
Dylan Musil <dmmusil@ncsu.edu>
2022-01-20 23:20:17 -0500

    Setup fly deploys.

--- lib/arcade_web/live/hangman_live/hangman.ex
+++ lib/arcade_web/live/hangman_live/hangman.ex
@@ -1,27 +1,32 @@
 defmodule ArcadeWeb.Hangman do
   use Phoenix.LiveView
-  use Phoenix.HTML
 
   def mount(%{"secret" => secret}, _session, socket) do
     socket =
       socket
       |> assign(:secret, secret)
       |> assign(:guesses, [])
+      |> assign(:misses, 0)
+      |> assign(:status, "")
 
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <h1><%= print_puzzle(assigns.secret, assigns.guesses) %></h1>
+    <div class="container">
+      <h1><%= print_puzzle(assigns.secret, assigns.guesses) %></h1>
 
-    <p>
-    <%= for c <- guessables() do %>
-    <span phx-key={c} phx-click='guess' phx-value-guess={c} style='cursor: pointer;'><%= c %></span>
-    <% end  %>
-    </p>
+      <p>
+        <%= for c <- guessables() do %>
+          <span phx-key={c} phx-click='guess' phx-value-guess={c} style='cursor: pointer;'><%= c %></span>
+        <% end  %>
+      </p>
 
-    <p>Guessed so far: <%= @guesses %></p>
+      <p><%= @status %></p>
+      <p>Guessed so far: <%= @guesses %></p>
+      <p>Incorrect guesses: <%= @misses %></p>
+    </div>
     """
   end
 
@@ -33,14 +38,21 @@ defmodule ArcadeWeb.Hangman do
     ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
   end
 
+  def handle_event("guess", _, %{assigns: %{misses: misses}} = socket) when misses >= 4 do
+    {:noreply, assign(socket, :status, "Game over")}
+  end
+
   def handle_event("guess", %{"guess" => c}, socket) do
-    currrnt_guesses = socket.assigns.guesses
-    guesses = if c in currrnt_guesses, do: currrnt_guesses, else: currrnt_guesses ++ [c]
+    current_guesses = socket.assigns.guesses
 
-    socket =
-      socket
-      |> assign(:guesses, guesses)
+    misses =
+      case c in String.codepoints(socket.assigns.secret) do
+        true -> socket.assigns.misses
+        _ -> socket.assigns.misses + 1
+      end
 
-    {:noreply, socket}
+    guesses = if c in current_guesses, do: current_guesses, else: current_guesses ++ [c]
+
+    {:noreply, assign(socket, %{:guesses => guesses, :misses => misses})}
   end
 end
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\arcade" eval Arcade.Release.migrate
