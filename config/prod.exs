77c95715d0074a50ab8eac8259df2b978d477aae
Dylan Musil <dmmusil@ncsu.edu>
2022-01-24 00:05:15 -0500

    More host fixes

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,11 @@ import Config
 config :arcade, ArcadeWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   url: [host: "elixir.musil.dev"],
-  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil, hsts: true]
+  force_ssl: [
+    rewrite_on: [:x_forwarded_host, :x_forwarded_port, :x_forwarded_proto],
+    host: nil,
+    hsts: true
+  ]
 
 # Do not print debug messages in production
 config :logger, level: :info
