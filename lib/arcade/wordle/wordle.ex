764ef08344f656b4342c1f9929a7cb96889cec26
Dylan Musil <dmmusil@ncsu.edu>
2022-01-25 09:05:11 -0500

    Refactor

--- lib/arcade/wordle/wordle.ex
+++ lib/arcade/wordle/wordle.ex
@@ -1,24 +1,17 @@
 defmodule Arcade.Wordle do
   def evaluate_guess(word, guess) do
-    letter_count =
-      word
-      |> String.codepoints()
-      |> Enum.reduce(%{}, fn char, map ->
-        Map.update(map, char, 1, fn v ->
-          cond do
-            is_nil(v) -> 1
-            true -> v + 1
-          end
-        end)
-      end)
+    letter_count = letter_count(word)
+
+    initial_state =
+      {%{"0" => :absent, "1" => :absent, "2" => :absent, "3" => :absent, "4" => :absent},
+       letter_count}
 
     correct_result =
       guess
       |> String.codepoints()
       |> Enum.with_index()
       |> Enum.reduce(
-        {%{"0" => :absent, "1" => :absent, "2" => :absent, "3" => :absent, "4" => :absent},
-         letter_count},
+        initial_state,
         fn {c, i}, acc ->
           {result, counts} = acc
 
@@ -32,26 +25,29 @@ defmodule Arcade.Wordle do
         end
       )
 
-    guess
-    |> String.codepoints()
-    |> Enum.with_index()
-    |> Enum.reduce(
-      correct_result,
-      fn {c, i}, acc ->
-        {result, counts} = acc
+    final_result =
+      guess
+      |> String.codepoints()
+      |> Enum.with_index()
+      |> Enum.reduce(
+        correct_result,
+        fn {c, i}, acc ->
+          {result, counts} = acc
 
-        cond do
-          Map.get(result, to_string(i)) == :correct ->
-            acc
+          cond do
+            Map.get(result, to_string(i)) == :correct ->
+              acc
 
-          String.contains?(word, c) && at_least_one(counts, c) ->
-            {Map.put(result, to_string(i), :present), decrement_letter(counts, c)}
+            String.contains?(word, c) && at_least_one(counts, c) ->
+              {Map.put(result, to_string(i), :present), decrement_letter(counts, c)}
 
-          true ->
-            acc
+            true ->
+              acc
+          end
         end
-      end
-    )
+      )
+
+    final_result
     |> elem(0)
     |> Map.values()
   end
@@ -62,5 +58,18 @@ defmodule Arcade.Wordle do
 
   defp at_least_one(counts, letter) do
     Map.get(counts, letter, 0) > 0
+  end
+
+  defp letter_count(word) do
+    word
+    |> String.codepoints()
+    |> Enum.reduce(%{}, fn char, map ->
+      Map.update(map, char, 1, fn v ->
+        cond do
+          is_nil(v) -> 1
+          true -> v + 1
+        end
+      end)
+    end)
   end
 end
