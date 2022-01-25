931040330dec9a8271cabd97b4a4dbbad4b427c6
Dylan Musil <dmmusil@ncsu.edu>
2022-01-25 10:23:24 -0500

    Fix test for home page

--- test/arcade_web/controllers/page_controller_test.exs
+++ test/arcade_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ArcadeWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to my blog"
   end
 end
