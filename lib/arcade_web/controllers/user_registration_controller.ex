90d5e86b0c3ebd0c079e0c89cdb785e5341eeabd
Dylan Musil <dmmusil@ncsu.edu>
2022-01-29 22:18:38 -0500

    Hacky way to disable registration.

--- lib/arcade_web/controllers/user_registration_controller.ex
+++ lib/arcade_web/controllers/user_registration_controller.ex
@@ -7,24 +7,29 @@ defmodule ArcadeWeb.UserRegistrationController do
 
   def new(conn, _params) do
     changeset = Accounts.change_user_registration(%User{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", changeset: changeset, disabled: true)
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.register_user(user_params) do
-      {:ok, user} ->
-        {:ok, _} =
-          Accounts.deliver_user_confirmation_instructions(
-            user,
-            &Routes.user_confirmation_url(conn, :edit, &1)
-          )
+    if Mix.env() == :prod do
+      changeset = Accounts.change_user_registration(%User{})
+      render(conn, "new.html", disabled: true, changeset: changeset)
+    else
+      case Accounts.register_user(user_params) do
+        {:ok, user} ->
+          {:ok, _} =
+            Accounts.deliver_user_confirmation_instructions(
+              user,
+              &Routes.user_confirmation_url(conn, :edit, &1)
+            )
 
-        conn
-        |> put_flash(:info, "User created successfully.")
-        |> UserAuth.log_in_user(user)
+          conn
+          |> put_flash(:info, "User created successfully.")
+          |> UserAuth.log_in_user(user)
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        {:error, %Ecto.Changeset{} = changeset} ->
+          render(conn, "new.html", changeset: changeset, disabled: false)
+      end
     end
   end
 end
